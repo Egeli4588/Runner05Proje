df208bb343dac9292081f0085657bd480bd5697d
Ali UYSUN <ali_uysun45@hotmail.com>
2025-08-05 15:30:44 +0300

    reStartmenu bitti

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,23 +5,39 @@ using UnityEngine.SceneManagement;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] PlayerController playerController;
-    [SerializeField] public GameObject gameStartMenu;
+    [SerializeField] public GameObject gameStartMenu;// start menü paneli için
+    [SerializeField] public GameObject gameRestartMenu;// Restart menü Paneli için
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        gameStartMenu.SetActive(true);// oyun baþladýðýnda startmenu paneli açýyor
+        gameRestartMenu.SetActive(false);// oyun baþladýðýnda reStartMenu panelini kapatýyor.
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerController.isDead)
+        {
+            gameRestartMenu.SetActive(true);
+
+        }
+
     }
-    public void StartGame() 
+    public void StartGame()
     {
-     playerController.isStart = true;// oyun baþladý
+        playerController.isStart = true;// oyun baþladý
         playerController.myAnim.SetBool("Run", true);
         gameStartMenu.SetActive(false);
     }
+
+    public void ReStartGame() 
+    {
+        // SceneManager.LoadScene(0); çok dinamik deðil
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);// daha dinamik yapýdýr.
+    
+    }
+
+
 }
