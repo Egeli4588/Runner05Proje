ae0fc3d87354c9dc487a4afe5d71708ec26f31ec
Ali UYSUN <ali_uysun45@hotmail.com>
2025-08-06 10:31:20 +0300

    enumı bağladık

---
+++ Assets/Scripts/Collectables.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Collectables : MonoBehaviour
+{
+    //Enuma eriþim saðlýyorum
+    public CollectablesEnum CollectablesEnum;  // enumýn içindeki objelere erþebilirim (Coin,Shield,...)
+
+    
+
+
+}
