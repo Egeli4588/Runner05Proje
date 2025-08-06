24313e607710c3adfb79f72bd55fe1e014fbb644
Ali UYSUN <ali_uysun45@hotmail.com>
2025-08-06 15:23:38 +0300

    game maneger hazır

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,9 +8,11 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] Transform roadParent;// dinamik olarak yollarý üzerinde oluþturacaðýmýz obje (holder)
 
+    [SerializeField] GameObject[] collactables; // dinamik olarak oluþturacaðým kodlarý tutacak olan dizi
 
-    
-    
+
+
+
     float roadLength = 20;
 
     int startRoadCount = 6;
@@ -18,25 +20,37 @@ public class GameManager : MonoBehaviour
     private void Start()
     {   //bu komut temelde klon oluþturmaya yarýyor.
         Instantiate(road[0], transform.position, Quaternion.identity, roadParent);
+
         for (int i = 0; i < startRoadCount; i++)
         {
             GenerateRoad();
-            
+
         }
+        SpawnCollectable();
+    }
+
+    void SpawnCollectable()
+    {
+        GameObject collectableObject = Instantiate(collactables[Random.Range(0, collactables.Length)], Player.position + new Vector3(0, 0.5f, 50f), Quaternion.identity);
+        Collectables collectable = collectableObject.GetComponent<Collectables>();
+        if (collectable.CollectablesEnum == CollectablesEnum.Coin) 
+        {
+            collectable.Player = Player.gameObject;
+        }
+        Invoke("SpawnCollectable", Random.Range(10f, 20f));
     }
 
     private void Update()
     {
-        if (Player.position.z > roadLength / 2) 
+        if (Player.position.z > roadLength / 2)
         {
             GenerateRoad();
         }
     }
-    void GenerateRoad() 
+    void GenerateRoad()
     {
         Instantiate(road[Random.Range(0, road.Length)], transform.position + new Vector3(0, 0, roadLength), Quaternion.identity, roadParent);
         roadLength += 20;
 
     }
 }
- 
